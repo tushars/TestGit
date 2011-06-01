7414ad50ede25ebce3a0660e0cc8c10845058aaf
tushars <tushar.m.s@gmail.com>
2011-06-01 14:10:48 -0700

    First Git project via EGit

---
+++ src/com/tushar/git/TestGit.java
@@ -0,0 +1,14 @@
+package com.tushar.git;
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+
+	}
+
+}
