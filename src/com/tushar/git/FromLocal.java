990eacba74caf61c2f316f35d494293e7e30cd7b
tushars <tushar.m.s@gmail.com>
2011-06-01 14:28:27 -0700

    Commiting another file from local repo

---
+++ src/com/tushar/git/FromLocal.java
@@ -0,0 +1,14 @@
+package com.tushar.git;
+
+public class FromLocal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("From local");
+	}
+
+}
